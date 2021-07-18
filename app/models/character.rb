ccb8a59b8cf13f9fb7dc2db0a2ce9c9719dc0ddc
Sylpha-Program <sylpha.program@gmail.com>
2021-07-18 12:12:01 +0900

    update character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ApplicationRecord
 
-  enum category: { '麦わらの一味': 0, '海賊1': 1, '海賊2': 2, '海軍': 3, 'その他': 4, 'DLC': 5}
-  enum action_type: { 'パワー': 0, 'スピード': 1, 'テクニック': 2, 'スカイ': 3}
+  enum category: { '麦わらの一味': 0, '海賊1': 1, '海賊2': 2, '海軍': 3, 'その他': 4, 'DLC': 5 }
+  enum action_type: { 'パワー': 0, 'スピード': 1, 'テクニック': 2, 'スカイ': 3 }
 
   has_many :scores
 
